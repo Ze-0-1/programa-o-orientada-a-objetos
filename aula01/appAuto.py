88b399228e5b00ee3d702ad5c3d7e9c45fad79a6
jose <jgabrielll2007@gmail.com>
2025-09-16 19:43:32 -0300

    Update appAuto.py

--- aula01/appAuto.py
+++ aula01/appAuto.py
@@ -1,35 +1,30 @@
 from flask import Flask, jsonify
 from aula01.montadora import MontadoraDAO
 
-
 app = Flask(__name__)
 
 
 @app.route('/montadoras_json', methods=['GET'])
 def montadoras_json():
-   dao = MontadoraDAO()
-   """
-   Rota para listar todas as montadoras do banco de dados.
-   Retorna uma resposta JSON.
-   """
-   if not dao.connection or not dao.connection.is_connected():
-      return jsonify({'erro': 'Não foi possível conectar ao banco de dados.'}), 500
+    dao = MontadoraDAO()
+    """
+    Rota para listar todas as montadoras do banco de dados.
+    Retorna uma resposta JSON.
+    """
+    if not dao.connection or not dao.connection.is_connected():
+        return jsonify({'erro': 'Não foi possível conectar ao banco de dados.'}), 500
 
+    montadoras = dao.get_montadoras()
 
-   montadoras = dao.get_montadoras()
-
-
-   if montadoras:
-       # Flask automaticamente serializa uma lista de dicionários para JSON
-       return jsonify(montadoras), 200
-   else:
-       # Se não houver montadoras ou ocorrer um erro na busca
-       return jsonify({'mensagem': 'Nenhuma montadora encontrada ou erro ao buscar dados.'}), 404
-
-
+    if montadoras:
+        # Flask automaticamente serializa uma lista de dicionários para JSON
+        return jsonify(montadoras), 200
+    else:
+        # Se não houver montadoras ou ocorrer um erro na busca
+        return jsonify({'mensagem': 'Nenhuma montadora encontrada ou erro ao buscar dados.'}), 404
 
 
 if __name__ == '__main__':
-   # Execute a aplicação Flask
-   # Em um ambiente de produção, você usaria um servidor WSGI como Gunicorn ou uWSGI
-   app.run(debug=True, port=5000)
+    # Execute a aplicação Flask
+    # Em um ambiente de produção, você usaria um servidor WSGI como Gunicorn ou uWSGI
+    app.run(debug=True, port=5000)
